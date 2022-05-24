b276a8d157ccee616400c173508e2a94234544d6
wttch（王冲） <wttch@wttch.com>
2022-05-24 22:12:49 +0800

    Initial Commit

---
+++ IOSTodoList/IOSTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTodoListApp.swift
+//  IOSTodoList
+//
+//  Created by Wttch on 2022/5/24.
+//
+
+import SwiftUI
+
+@main
+struct IOSTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
